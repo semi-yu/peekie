e0b02da98d6b8b524677edb8187948ab9b457849
armedHeistKino <urban.manul.1919@gmail.com>
2024-12-04 12:17:08 +0900

    chore: Integrate library file includes into one file

---
+++ src/peekie.hpp
@@ -0,0 +1,4 @@
+#include "./core/input_handler.hpp"
+#include "./mouse/interface_mouse_subscriber.hpp"
+#include "./keyboard/interface_keyboard_subscriber.hpp"
+#include "./window/interface_window_subscriber.hpp"
