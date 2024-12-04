7c9e194c1130efb29e9fa388b3c181b14f85660e
armedHeistKino <urban.manul.1919@gmail.com>
2024-12-04 11:55:38 +0900

    add: Adapt header separation to example source code1

--- src/example/example1.cpp
+++ src/example/example1.cpp
@@ -10,11 +10,10 @@
 #include <fmt/core.h>
 #include <fmt/color.h>
 
-#include "../core/input_handler.cpp"
-
+#include "../core/input_handler.hpp"
 #include "../mouse/interface_mouse_subscriber.hpp"
-#include "../keyboard/interface_keyboard_subscriber.cpp"
-#include "../window/interface_window_subscriber.cpp"
+#include "../keyboard/interface_keyboard_subscriber.hpp"
+#include "../window/interface_window_subscriber.hpp"
 
 namespace other {
     class Thing
